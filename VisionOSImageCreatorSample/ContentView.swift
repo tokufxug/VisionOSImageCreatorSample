1b8dc4a524bf0494eccd1c1051d1598dc69734c2
tokufxug <tokufxug@gmail.com>
2025-02-22 16:08:20 +0900

    first commit

--- VisionOSImageCreatorSample/ContentView.swift
+++ VisionOSImageCreatorSample/ContentView.swift
@@ -6,18 +6,87 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
+import ImagePlayground
 
 struct ContentView: View {
+    
+    @Environment(\.supportsImagePlayground) private var supportsImagePlayground
+    @State private var generatedImages: [Image] = []
+    @State private var isLoading = false
+    @State private var inputText: String = "A cat wearing mittens"
+    @State private var generatedText: String = ""
+    
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+            if !generatedImages.isEmpty {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(generatedImages.indices, id: \..self) { index in
+                            generatedImages[index]
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 300, height: 300)
+                            }
+                        }
+                }
+            } else {
+                if ImagePlaygroundViewController.isAvailable && supportsImagePlayground {
+                    Text("No image generated")
+                        .foregroundColor(.gray)
+                } else {
+                    Text("ImagePlayground unavailable")
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            TextField("Enter description (e.g. A cat wearing mittens)", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                Task {
+                    await generateImage()
+                }
+            }) {
+                Text("Generate Image")
+                    .padding()
+            }
+            .disabled(!ImagePlaygroundViewController.isAvailable || !supportsImagePlayground || inputText.isEmpty || isLoading)
+            
+            if !generatedText.isEmpty {
+                Text(generatedText)
+            }
         }
         .padding()
+    }
+    
+    @MainActor
+    func generateImage() async {
+        isLoading = true
+        generatedImages.removeAll()
+        generatedText = ""
+        
+        do {
+            let creator = try await ImageCreator()
+            guard let style = creator.availableStyles.first else { return }
+            
+            let images = creator.images(
+                for: [.text(inputText)],
+                    style: style,
+                    limit: 4)
+            
+            generatedText = inputText
+            
+            for try await image in images {
+                let uiImage = UIImage(cgImage: image.cgImage)
+                generatedImages.append(Image(uiImage: uiImage))
+            }
+            
+        } catch {
+            generatedText = "Failed to generate image: \(error) : \(inputText)"
+        }
+        isLoading = false
+        inputText = ""
     }
 }
 
