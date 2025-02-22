3ce30ead7edd9c45204f4157245bb18b173b35fc
tokufxug <tokufxug@gmail.com>
2025-02-22 14:40:49 +0900

    Initial Commit

---
+++ VisionOSImageCreatorSample/VisionOSImageCreatorSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSImageCreatorSampleApp.swift
+//  VisionOSImageCreatorSample
+//
+//  Created by Sadao Tokuyama on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSImageCreatorSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOSImageCreatorSampleTests/VisionOSImageCreatorSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSImageCreatorSampleTests.swift
+//  VisionOSImageCreatorSampleTests
+//
+//  Created by Sadao Tokuyama on 2/22/25.
+//
+
+import Testing
+@testable import VisionOSImageCreatorSample
+
+struct VisionOSImageCreatorSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
